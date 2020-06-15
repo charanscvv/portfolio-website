dc44ea08c31c02d16ff96d3fed3aa5b021704d76
charanscvv <velpur.charan@gmail.com>
2020-06-15 03:39:46 -0400

    Contact page

--- ui/src/app/contact/contact.component.ts
+++ ui/src/app/contact/contact.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy  } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+export class ContactComponent implements OnInit,OnDestroy  {
+  shake;
+  constructor() {
+   }
+   
   ngOnInit(): void {
+      this.shake = setInterval(() => {
+        document.getElementsByClassName('contact')[0].classList.toggle('animate');
+      }, 800);
+    
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.shake);
   }
 
 }
